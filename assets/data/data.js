9cfd2199f78055762cf086dc5093d639a0fd354d
Ammad-Cherrybyte <63580683+ammad-ahmed1@users.noreply.github.com>
2024-07-09 18:19:36 +0500

    designing

---
+++ assets/data/data.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    id: 8,
+    type: "DEPOSIT",
+    date: "12/03/2020",
+    amount: "1300,00 €",
+  },
+  {
+    id: 7,
+    type: "DEPOSIT",
+    date: "08/03/2020",
+    amount: "79,97 €",
+  },
+  {
+    id: 6,
+    type: "WITHDRAWAL",
+    date: "23/12/2019",
+    amount: "-133,90 €",
+  },
+  {
+    id: 5,
+    type: "WITHDRAWAL",
+    date: "19/11/2019",
+    amount: "-642,21 €",
+  },
+  {
+    id: 4,
+    type: "DEPOSIT",
+    date: "12/07/2019",
+    amount: "25 000,00 €",
+  },
+  {
+    id: 3,
+    type: "WITHDRAWAL",
+    date: "27/05/2019",
+    amount: "-306,50 €",
+  },
+  {
+    id: 2,
+    type: "DEPOSIT",
+    date: "01/04/2019",
+    amount: "455,23 €",
+  },
+  {
+    id: 1,
+    type: "DEPOSIT",
+    date: "28/01/2019",
+    amount: "200,00 €",
+  },
+];
